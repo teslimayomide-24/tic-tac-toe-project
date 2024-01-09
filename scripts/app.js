1f2ec543ade9078dd92dcf7cbad5ae60d01c459b
teslimayomide-24 <teslimayomide.24@gmail.com>
2024-01-09 20:16:38 +0100

    code for storing and managing submitted data

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@
 
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
@@ -13,4 +15,6 @@ editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 
 cancelOverlayBtnElement.addEventListener('click', cancelConfigOverlay);
-backdropElement.addEventListener('click', cancelConfigOverlay)
+backdropElement.addEventListener('click', cancelConfigOverlay);
+
+formElement.addEventListener('submit', savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,20 @@ function openPlayerConfig() {
 function cancelConfigOverlay() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
+};
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim();
+
+    if (!enteredPlayername) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputElement.textContent = 'Please entere a valid name!';
+        return;
+    }
+
+    
 };
