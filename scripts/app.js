df383e03116a53dad2f32e2984ec2940d7f0e193
teslimayomide-24 <teslimayomide.24@gmail.com>
2024-01-12 13:17:11 +0100

    tracking selected fields on the game board

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,9 @@
 // Adding eventlisteners
+
+let editedPlayer = 0;
+let activePlayer = 0;
+
+
 const players = [
     {
         name: '',
@@ -15,11 +20,15 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 
 const cancelOverlayBtnElement = document.getElementById('cancel-overlay');
+const startNewGameBtnElement = document.getElementById('start-game-btn');
+const gameFieldElements = document.querySelectorAll('#game-board li');
 
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
@@ -29,3 +38,9 @@ cancelOverlayBtnElement.addEventListener('click', cancelConfigOverlay);
 backdropElement.addEventListener('click', cancelConfigOverlay);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
+
+for (const gameFieldElement of gameFieldElements) {
+    gameFieldElement.addEventListener('click', selectGameField);
+}
